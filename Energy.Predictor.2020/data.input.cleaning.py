3ba232afde8260d9209664fa79b0d9838aa48bb6
Douglas Thoms <douglasthoms@rodanenergy.com>
2020-04-03 13:26:48 -0400

    start of energy predictor project

---
+++ Energy.Predictor.2020/data.input.cleaning.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 12:58:13 2020
+Input of data
+@author: dthoms
+"""
+
+import numpy as np
+import pandas as pd
+
+"""
+Input Data
+"""
+
+"""
+set up API to automatically date and download"
+"""
+
+
+
+
+buildingMetaData = pd.read_csv("kaggle-data/building_metadata.csv")
+buildingMetaData = pd.read_csv("kaggle-data/building_metadata.csv")
+
+print(buildingMetaData)
