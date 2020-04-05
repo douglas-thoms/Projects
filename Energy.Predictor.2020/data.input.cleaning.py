56d2cf1a2c33a92d68616340016c5affd6dab5ad
Douglas Thoms <douglasthoms@rodanenergy.com>
2020-04-04 23:22:45 -0400

    start EDA of data, memory requirements

--- Energy.Predictor.2020/data.input.cleaning.py
+++ Energy.Predictor.2020/data.input.cleaning.py
@@ -20,6 +20,18 @@ set up API to automatically date and download"
 
 
 buildingMetaData = pd.read_csv("kaggle-data/building_metadata.csv")
-buildingMetaData = pd.read_csv("kaggle-data/building_metadata.csv")
+trainData = pd.read_csv("kaggle-data/train.csv")
+trainWeatherData = pd.read_csv("kaggle-data/weather_train.csv")
+
+bMDSample = buildingMetaData.head(100)
+trainSample = trainData.head(100)
+weathTrainSample = trainWeatherData.head(100)
+
+test = pd.merge(trainData,buildingMetaData,how='outer',on='building_id')
+
+test = pd.merge(test,trainWeatherData,how='outer',on='site_id')
+testSample = test.head(100)
 
 print(buildingMetaData)
+
+#Study spider reports and pweave
