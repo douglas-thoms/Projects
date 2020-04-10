929fbd07526ec9e49f8793f4a4faae9578325da6
Douglas Thoms <douglasthoms@rodanenergy.com>
2020-04-10 00:12:37 -0400

    remove nan

--- Energy.Predictor.2020/data.input.cleaning.py
+++ Energy.Predictor.2020/data.input.cleaning.py
@@ -36,26 +36,16 @@ trainWeatherData = trainWeatherData[i1.isin(i2)]
 #combine combDF with revised trainWeatherData
 combDF = pd.merge(combDF,trainWeatherData,how='outer',on=['site_id','timestamp'])
 
+combDF.drop(columns = ['year_built','floor_count','cloud_coverage','precip_depth_1_hr'],
+            inplace=True)
+
+combDF.dropna(inplace = True)
+
+#output csv
+combDF.to_csv("data/combDF.csv", index = False)
+
 #create samples
 bMDSample = bMetaData.head(100)
 trainSample = trainData.head(100)
 weathTrainSample = trainWeatherData.head(100)
 combDFSample = combDF.head(100)
-
-#output csv
-combDF.to_csv("data/combDF.csv", index = False)
-
-#trainWeatherData has irrelevant unused observations, remove observations
-keys = list(['site_id','timestamp'])
-i1 = trainWeatherData.set_index(keys).index
-i2 = combDF.set_index(keys).index
-trainWeatherData = trainWeatherData[i1.isin(i2)]
-
-#combine combDF with revised trainWeatherData
-combDF = pd.merge(combDF,trainWeatherData,how='outer',on=['site_id','timestamp'])
-
-#create samples
-bMDSample = bMetaData.head(100)
-trainSample = trainData.head(100)
-weathTrainSample = trainWeatherData.head(100)
-
