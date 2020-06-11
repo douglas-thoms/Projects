0b6c929fe990f98f5206e76fb8978d3708647fdb
Douglas Thoms <douglasthoms@rodanenergy.com>
2020-06-11 09:49:15 -0400

    updating remove script

--- Energy.Predictor.2020/data.cleaning.py
+++ Energy.Predictor.2020/data.cleaning.py
@@ -9,9 +9,13 @@ Created on Sun Jun  7 08:04:17 2020
 #remove building and meter data because not legitimate data
 #set data as list
 
-def remove_building_meter(file,list):
-    for entry in list:
-        file.drop(file[(file['building_id'] == entry[0]) & (file['meter'] == entry[1])].index,inplace=True)
+def remove_building_meter(file,bad_meters):
+    #tuples make index shift by 1
+    for row in bad_meters.itertuples():
+        print(row[2])
+        file.drop(file[(file['building_id'] == row[1]) & (file['meter'] == row[2])].index,inplace=True)
+    #  for entry in list:
+   #     file.drop(file[(file['building_id'] == entry[0]) & (file['meter'] == entry[1])].index,inplace=True)
 
 
 
@@ -21,6 +25,8 @@ import pandas as pd
 import gc
 
 combDF = pd.read_csv('data/combDF.csv')
+
+bad_meters = pd.read_csv('data_cleaning/too_low_electricity_iter1.csv')
 
 combDF.drop(columns = ['year_built','floor_count','cloud_coverage','precip_depth_1_hr'],
             inplace=True)
@@ -38,5 +44,5 @@ combDF.dropna(inplace = True)
 # combDF.drop(combDF[(combDF['building_id'] == 1159) & (combDF['meter'] == 2)].index,inplace=True)
 # combDF.drop(combDF[(combDF['building_id'] == 1099) & (combDF['meter'] == 2)].index,inplace=True)   
 
-building_meter_remove = [[1099,2], [778,1], [1021,3],[1168,2],[1197,2],[1148,2],[1159,2],[1099,2]]
-remove_building_meter(combDF, building_meter_remove)
+#building_meter_remove = [[1099,2], [778,1], [1021,3],[1168,2],[1197,2],[1148,2],[1159,2],[1099,2]]
+remove_building_meter(combDF, bad_meters)
